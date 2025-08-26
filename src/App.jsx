ceb9c23a62fcd0979eedcd9fa2102e43ab6c53cd
Jasontran880 <152024215+Jasontran880@users.noreply.github.com>
2025-08-26 17:28:57 -0400

    Setup API call function & API error handling
    Setup secure API key in seperate file within project.
    
    Setup API_OPTIONS object which acts as a bridge of authentication and
    ensuring proper data passing from API endpoint to program.
    
    Setup API error handling for the API call function and display the error
    in the console as well as on the screen.
    
    Setup useEffect hook which will be the section of the code in which
    we call the API call function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
+
+// Handling Movie Database API  
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+// reference to the private API Key for API requests
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    // API returns json objects
+    accept: 'application/json',
+    // Verifies WHO is trying to make the API request
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => { 
   // Create a search state within the main application and pass the state fields as props to the Search component, for the sake of global scope
   const [searchTerm, setSearchTerm] = useState('');
+
+  // to display the error of a program on the screen
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Function for calling API
+  // Use Async function for the 'await' keyword: suspends function's execution until the Promise settles: either resolves with a value or rejects with an error
+  const fetchMovies = async () => {
+    try {
+      // set the endpoint (the api url you are trying to recieve info from)
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+      // call the api
+      const response = await fetch(endpoint, API_OPTIONS)
+    } catch (error){
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later baka');
+    }
+  }
+
+
+
+  // We only want this to load once at the beginning of the program
+  //
+  useEffect(()=> {
+
+  }, [])
+  // the empty array above is an empty dependency array which says that the effect will only activate if the values in the dependency array change
+
   return (
     <main>
       <div className="pattern" />
@@ -13,11 +56,24 @@ const App = () => {
         <header>  
            <img src="./hero.png" alt="Hero Banner"/>
           <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle </h1>
+        
+          {/* Search component for movie search */}
+          {/* Pass state fields as props into Search component */}
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        
         </header>
 
-        {/* Search component for movie search */}
-        {/* Pass state fields as props into Search component */}
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+
+          <h2>All Movies </h2>
+
+
+          {/* Conditional rendering of errorMessage */}
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+
+        </section>
+        
 
       </div>
       
